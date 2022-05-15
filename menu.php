40b2c6a48f9ccdbb5b9e6d4ba91dcd5f18f7dac3
iamllcoolray <llcoolray24@gmail.com>
2022-05-15 14:08:37 -0400

    added content to menu.php

--- menu.php
+++ menu.php
@@ -1,9 +1,114 @@
 <?php $page_name = "Menu" ?>
 <?php require("includes/header.php") ?>
 <div class="container mt-5">
-    <div class="pt-5 text-center">>
+    <div class="my-5 pt-5 text-center">
         <h3 class="heading neonText">Menu</h3>
     </div>
-
+    <div class="pb-5 row text-center">
+        <section id="kava" class="col-lg-6">
+            <h4 class="pb-3">Kava</h4>
+            <p>$5/$9 - Dragons Blood <br> (Kava with Dragon Fruit)</p>
+            <p>$4/$7 - Borogu <br> (Vanuatu)</p>
+            <p>$6/10 - 20 Year <br> (Hawaii)</p>
+            <p>$8/$13 - Kraft <br> (Fiji)</p>
+            <p>$13 - Dawn of the Dead</p>
+            <p>$6 - Leilo Kava Soda <br> (Lemon Ginger & Raspberry Hibiscus)</p>
+        </section>
+        <section id="coffee-drinks" class="col-lg-6 ms-auto">
+            <h4 class="pb-3">Coffee Drinks</h4>
+            <div class="row">
+                <div class="col-lg-6">
+                    <h5>Cold Brews</h5>
+                    <p>* Try it mixed with the Django for a Kold Brew *</p>
+                    <p>New Orleans Style Cold Brew <br> (Dark with Chicory)</p>
+                    <p>Light Roast Cold Brew <br> (Roasted in House)</p>
+                </div>
+                <div class="col-lg-6 ms-auto">
+                    <h5>Espresso</h5>
+                    <p>Basic <br> (Lavazza)</p>
+                    <p>Supreme <br> (Rotating, ask server)</p>
+                </div>
+            </div>
+        </section>
+    </div>
+    <section id="happy-teas" class="text-center">
+        <h4 class="pb-3">Happy Teas</h4>
+        <div class="mb-3 row">
+            <div class="col-lg-6">
+                <h5>Green Happy Teas</h5>
+                <p>OG - Unflavored</p>
+                <p>You're Turning Violet, Violet <br> (Blueberry, Lavender, Vanilla & Lime)</p>
+                <p>Enter ther Dragonfruit <br> (Raspberry & Dragon Fruit)</p>
+                <p>Twisted Pair <br> (Strawberry, Passion Fruit, Lulo)</p>
+            </div>
+            <div class="col-lg-6">
+                <h5>White Happy Teas</h5>
+                <p>OG - Unflavored</p>
+                <p>Fateful Findings <br> (Lulo, Mango & Passion Fruit)</p>
+                <p>Django! <br> (Cacao & Cinnamon)</p>
+                <p>Suspiria <br> (Hibiscus, Raspberry & Rose Water)</p>
+            </div>
+            <div class="col-lg-6">
+                <h5>Red Happy Teas</h5>
+                <p>OG - Unflavored</p>
+                <p>Double Down <br> (Chicha Morada, Lime & Sugar)</p>
+                <p>Stellastarr* <br> (Strawberry & 5 Spices)</p>
+                <p>Creating Rem Lazar <br> (White Grapefuit & Lavender)</p>
+            </div>
+            <div class="col-lg-6">
+                <h5>Tea Extract Mixers</h5>
+                <p>* Any soda can also be mixed with extract *</p>
+                <p>Jakarta & Coke <br> (Coke)</p>
+                <p>New Fashioned <br> (O.F. Mix, Tonic & Orange)</p>
+                <p>Saigon Mule <br> (Ginger Beer)</p>
+                <p>Tea & Tonic <br> (Tonic Water)</p>
+                <p>Kosmopolitan <br> (Cosmo & Tonic)</p>
+                <p>Markarita <br> (Margarita Mix & Tonic)</p>
+                <p>Limoncello <br> (Lemon Drop Mix & Tonic)</p>
+                <p>Endgame <br> (OG, 7ml, Grapefruit & Tonic)</p>
+                <p>Apocalypto <br> (Limoncello, Orange & Grapefruit)</p>
+            </div>
+        </div>
+        <p>(Extract Boost $3, OGs $5, Flavored Teas $6, Mocktails $7, Apocalypto $8, and Endgame $13)</p>
+    </section>
+    <section id="other-beverages" class="pb-5 text-center border-top">
+        <h4 class="pt-5 pb-3">Other Beverages</h4>
+        <div class="row">
+            <div class="col-lg-6">
+                <h5>Hot Teas</h5>
+                <p>Chamomile</p>
+                <p>Earl Grey</p>
+                <p>English Breakfast</p>
+                <p>Ginger</p>
+                <p>Green Tea</p>
+                <p>Hibiscus</p>
+                <p>Mate</p>
+                <p>Moringa</p>
+                <p>Oolong</p>
+                <p>Rooibos</p>
+            </div>
+            <div class="col-lg-6">
+                <h5>Soda</h5>
+                <p>Coke</p>
+                <p>Sprite</p>
+                <p>Moutain Dew</p>
+                <p>MD Gingerbread</p>
+                <p>MD VooDew</p>
+                <p>Diet Coke</p>
+                <p>Sunkist Blue Lemonade</p>
+                <p>Cherry Coke</p>
+                <p>Vernors Ginger</p>
+            </div>
+            <div class="col-lg-6">
+                <h5>Pitchers</h5>
+                <p>$31 - Kava or Mocktails</p>
+                <p>$26 - Happy Teas</p>
+            </div>
+            <div class="col-lg-6">
+                <h5>Check Out</h5>
+                <p>Kristen's Kravings Menu for Edibles & Sweets</p>
+            </div>
+        </div>
+    </section>
     <?php require("includes/footer.php") ?>
 </div>
