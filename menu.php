a2a7d359074a84435fa886b4e74f8295aac34051
iamllcoolray <llcoolray24@gmail.com>
2022-05-15 14:39:47 -0400

    added more styling to menu.php

--- menu.php
+++ menu.php
@@ -7,6 +7,7 @@
     <div class="pb-5 row text-center">
         <section id="kava" class="col-lg-6">
             <h4 class="pb-3">Kava</h4>
+            <h5 class="py-3 neonTextGreen">Shells</h5>
             <p>$5/$9 - Dragons Blood <br> (Kava with Dragon Fruit)</p>
             <p>$4/$7 - Borogu <br> (Vanuatu)</p>
             <p>$6/$10 - 20 Year <br> (Hawaii)</p>
@@ -18,13 +19,13 @@
             <h4 class="pb-3">Coffee Drinks</h4>
             <div class="row">
                 <div class="col-lg-6">
-                    <h5>Cold Brews</h5>
+                    <h5 class="py-3 neonTextRed">Cold Brews</h5>
                     <p>* Try it mixed with the Django for a Kold Brew *</p>
                     <p>New Orleans Style Cold Brew <br> (Dark with Chicory)</p>
                     <p>Light Roast Cold Brew <br> (Roasted in House)</p>
                 </div>
                 <div class="col-lg-6 ms-auto">
-                    <h5>Espresso</h5>
+                    <h5 class="py-3 neonTextOrange">Espresso</h5>
                     <p>Basic <br> (Lavazza)</p>
                     <p>Supreme <br> (Rotating, ask server)</p>
                 </div>
@@ -35,28 +36,28 @@
         <h4 class="pb-3">Happy Teas</h4>
         <div class="mb-3 row">
             <div class="col-lg-6">
-                <h5>Green Happy Teas</h5>
+                <h5 class="py-3 neonTextGreen">Green Happy Teas</h5>
                 <p>OG - Unflavored</p>
                 <p>You're Turning Violet, Violet <br> (Blueberry, Lavender, Vanilla & Lime)</p>
                 <p>Enter ther Dragonfruit <br> (Raspberry & Dragon Fruit)</p>
                 <p>Twisted Pair <br> (Strawberry, Passion Fruit, Lulo)</p>
             </div>
             <div class="col-lg-6">
-                <h5>White Happy Teas</h5>
+                <h5 class="py-3 neonTextWhite">White Happy Teas</h5>
                 <p>OG - Unflavored</p>
                 <p>Fateful Findings <br> (Lulo, Mango & Passion Fruit)</p>
                 <p>Django! <br> (Cacao & Cinnamon)</p>
                 <p>Suspiria <br> (Hibiscus, Raspberry & Rose Water)</p>
             </div>
             <div class="col-lg-6">
-                <h5>Red Happy Teas</h5>
+                <h5 class="py-3 neonTextRed">Red Happy Teas</h5>
                 <p>OG - Unflavored</p>
                 <p>Double Down <br> (Chicha Morada, Lime & Sugar)</p>
                 <p>Stellastarr* <br> (Strawberry & 5 Spices)</p>
                 <p>Creating Rem Lazar <br> (White Grapefuit & Lavender)</p>
             </div>
             <div class="col-lg-6">
-                <h5>Tea Extract Mixers</h5>
+                <h5 class="py-3 neonTextOrange">Tea Extract Mixers</h5>
                 <p>* Any soda can also be mixed with extract *</p>
                 <p>Jakarta & Coke <br> (Coke)</p>
                 <p>New Fashioned <br> (O.F. Mix, Tonic & Orange)</p>
@@ -75,7 +76,7 @@
         <h4 class="pt-5 pb-3">Other Beverages</h4>
         <div class="row">
             <div class="col-lg-6">
-                <h5>Hot Teas</h5>
+                <h5 class="py-3 neonTextGreen">Hot Teas</h5>
                 <p>Chamomile</p>
                 <p>Earl Grey</p>
                 <p>English Breakfast</p>
@@ -88,7 +89,7 @@
                 <p>Rooibos</p>
             </div>
             <div class="col-lg-6">
-                <h5>Soda</h5>
+                <h5 class="py-3 neonTextOrange">Soda</h5>
                 <p>Coke</p>
                 <p>Sprite</p>
                 <p>Moutain Dew</p>
@@ -100,12 +101,12 @@
                 <p>Vernors Ginger</p>
             </div>
             <div class="col-lg-6">
-                <h5>Pitchers</h5>
+                <h5 class="py-3 neonText">Pitchers</h5>
                 <p>$31 - Kava or Mocktails</p>
                 <p>$26 - Happy Teas</p>
             </div>
             <div class="col-lg-6">
-                <h5>Check Out</h5>
+                <h5 class="py-3 neonTextRed">Check Out</h5>
                 <p>Kristen's Kravings Menu for Edibles & Sweets</p>
             </div>
         </div>
