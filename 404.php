250fe9a1677206cd25de9e6a0af5aa2bffad48c5
Rayhan Alcena <llcoolray24@gmail.com>
2023-06-06 13:21:53 -0400

    fixed spacing issue

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div class="container mt-5">
 
     <section class="mb-5 py-5 text-center neonFlickerText">
-        <h1 class="heading404">404</h1>
+        <h1 class="heading404 mt-5 pt-5">404</h1>
         <h3 class="heading">PAGE NOT FOUND</h3>
     </section>
     <?php require("includes/footer.php") ?>
