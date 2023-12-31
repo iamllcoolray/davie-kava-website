f3daba4f21cf259b6cb14b1768e066b242ecf362
iamllcoolray <llcoolray24@gmail.com>
2023-12-31 16:02:21 -0500

    updated the menu

--- menu.php
+++ menu.php
@@ -4,72 +4,74 @@
     <div class="my-5 pt-5 text-center">
         <h3 class="heading neonText">Menu</h3>
     </div>
-    <div class="pb-5 row text-center">
-        <section id="kava" class="col-lg-6">
-            <h4 class="pb-3">Kava</h4>
-            <img src="img/specialty-drinks/kava-shell.png" alt="" class="col-lg-4 img-fluid" width="200" height="225">
-            <h5 class="py-3 neonTextGreen">Shells</h5>
-            <p>$5/$9 - Dragons Blood <br> (Kava with Dragon Fruit)</p>
-            <p>$5/$9 - Samoa</p>
-            <p>$4/$7 - Borogu <br> (Vanuatu)</p>
-            <p>$4/$7 - Mindy <br> (Van - Yalo)</p>
-            <p>$4/$7 - Body <br> (Van - Laloma)</p>
-            <p>$4/$7 - 50/50 <br> (Blend of Mindy & Body)</p>
-            <p>$6/$10 - 20 Year <br> (Hawaii)</p>
-            <p>$8/$13 - Kraft <br> (Fiji)</p>
-            <p>$13 - Dawn of the Dead</p>
-            <p>$6 - Leilo Kava Soda <br> (Lemon Ginger & Raspberry Hibiscus)</p>
-        </section>
-        <section id="coffee-drinks" class="col-lg-6 ms-auto">
-            <h4 class="pb-3">Coffee Drinks</h4>
-            <img src="img/specialty-drinks/cold-brew.png" alt="" class="col-lg-4 img-fluid" width="200" height="225">
-            <div class="row">
-                <div class="col-lg-6">
-                    <h5 class="py-3 neonTextRed">Cold Brews</h5>
-                    <p>* Try it mixed with the Django for a Kold Brew *</p>
-                    <p>New Orleans Style Cold Brew <br> (Dark with Chicory)</p>
-                    <p>Light Roast Cold Brew <br> (Roasted in House)</p>
-                </div>
-                <div class="col-lg-6 ms-auto">
-                    <h5 class="py-3 neonTextOrange">Espresso</h5>
-                    <p>Basic <br> (Lavazza)</p>
-                    <p>Supreme <br> (Rotating, ask server)</p>
-                </div>
-                <p>
-                    (We only use Oat Milk for dairy)
-                </p>
+    <section id="kava" class="pb-5 text-center">
+        <h4 class="pb-3">Kava</h4>
+        <img src="img/specialty-drinks/kava-shell.png" alt="" class="col-lg-4 img-fluid" width="200" height="225">
+        <h5 class="py-3 neonTextGreen">Shells</h5>
+        <p>$4/$7 - HOUSE KAVA <br> Mind (Yalo) <br> Body (Laloma) <br> 50/50 (Blend of Mind & Body)</p>
+        <!--<p>$4/$7 - Borogu <br> (Vanuatu)</p>
+        <p>$5/$9 - Dragons Blood <br> (Kava with Dragon Fruit)</p>
+        <p>$5/$9 - Samoa</p>
+        <p>$6/$10 - 20 Year <br> (Hawaii)</p>
+        <p>$8/$13 - Kraft <br> (Fiji)</p>-->
+        <p>$9- FLAVORED KAVA <br> Peppermint, Amaretto, Butterbeer, Pina Colada, Raspberry Creme, Caramel Macchiato, Maple Creme, & Latte Pronto</p>
+        <p>$7 - KAVA SHOTS <br> Maple</p>
+        <p>$13 - DAWN OF THE DEAD <br> (Kava w/ Kava Concentrate)</p>
+        <p>$6 - LEILO KAVA SODA</p>
+        <p>$7 - BBC KTEA SODA</p>
+        <p>$8 - D8 SODA</p>
+    </section>
+    <section id="coffee-drinks" class="pb-5 text-center border-top">
+        <h4 class="pt-5 pb-3">Coffee Drinks</h4>
+        <img src="img/specialty-drinks/cold-brew.png" alt="" class="col-lg-4 img-fluid" width="200" height="225">
+        <div class="row">
+            <div class="col-lg-6">
+                <h5 class="py-3 neonTextRed">Hot Drinks</h5>
+                <p>$3 - ESPRESSO <br> (Lavazza)</p>
+                <p>$5 - LATTE CAPPUCCINO <br> (Espresso & Oat Milk)</p>
+                <p>$3 - TURKISH</p>
             </div>
-        </section>
-    </div>
-    <section id="happy-teas" class="text-center">
-        <h4 class="pb-3">Happy Teas</h4>
+            <div class="col-lg-6 ms-auto">
+                <h5 class="py-3 neonTextWhite">Cold Drinks</h5>
+                <p>$5/$9 - COLD BREW</p>
+                <p>$7/$13 - FLAVORED COLD BREW <br> Caramel</p>
+            </div>
+            <p>
+                (We only use Oat Milk)
+            </p>
+        </div>
+    </section>
+    <section id="happy-teas" class="text-center border-top">
+        <h4 class="pt-5 pb-3">Happy Teas</h4>
         <img src="img/specialty-drinks/blue-happy-tea.png" alt="" class="col-lg-4 img-fluid" width="200" height="225">
         <div class="mb-3 row">
             <div class="col-lg-6">
                 <h5 class="py-3 neonTextGreen">Green Happy Teas</h5>
                 <p>OG - Unflavored</p>
-                <p>You're Turning Violet, Violet <br> (Blueberry, Lavender, Vanilla & Lime)</p>
-                <p>Enter ther Dragonfruit <br> (Raspberry & Dragon Fruit)</p>
+                <p>YOU'RE TURNING VIOLET, VIOLET <br> (Blueberry, Lavender, Vanilla & Lime)</p>
+                <p>ENTER THE DRAGONFRUIT <br> (Raspberry & Dragon Fruit)</p>
                 <p>Twisted Pair <br> (Strawberry, Passion Fruit, Lulo)</p>
             </div>
             <div class="col-lg-6">
                 <h5 class="py-3 neonTextWhite">White Happy Teas</h5>
                 <p>OG - Unflavored</p>
-                <p>Fateful Findings <br> (Lulo, Mango & Passion Fruit)</p>
-                <p>Django! <br> (Cacao & Cinnamon)</p>
-                <p>Suspiria <br> (Hibiscus, Raspberry & Rose Water)</p>
-                <p>White Kold Brew <br> (Cold Brewed Tea)</p>
+                <p>FATEFUL FINDINGS! <br> (Lulo, Mango & Passion Fruit)</p>
+                <p>DJANGO! <br> (Cacao & Cinnamon)</p>
+                <p>SUSPIRIA <br> (Hibiscus, Raspberry & Rose Water)</p>
+                <!--<p>White Kold Brew <br> (Cold Brewed Tea)</p>-->
+                <p>THE FORCE <br> (Lemondae Gatorade Tea)</p>
             </div>
             <div class="col-lg-6">
                 <h5 class="py-3 neonTextRed">Red Happy Teas</h5>
                 <p>OG - Unflavored</p>
-                <p>Samurai Cop <br> (Orange & Orange Flower)</p>
-                <p>Double Down <br> (Chicha Morada, Lime & Sugar)</p>
-                <p>Stellastarr* <br> (Strawberry & 5 Spices)</p>
-                <p>Creating Rem Lazar <br> (White Grapefuit & Lavender)</p>
-                <p>Red Kold Brew <br> (Cold Brewed Tea)</p>
+                <p>SAMURAI COP <br> (Orange & Orange Flower)</p>
+                <p>DOUBLE DOWN <br> (Chicha Morada, Lime & Sugar)</p>
+                <p>STELLASTARR* <br> (Strawberry & 5 Spices)</p>
+                <!--<p>Creating Rem Lazar <br> (White Grapefuit & Lavender)</p>
+                <p>Red Kold Brew <br> (Cold Brewed Tea)</p>-->
+                <p>MOS EISLEY LEMONADE <br> (Pink Lemonade Tea)</p>
             </div>
-            <div class="col-lg-6">
+            <!--<div class="col-lg-6">
                 <h5 class="py-3 neonTextOrange">Tea Extract Mixers</h5>
                 <p>* Any soda can also be mixed with extract *</p>
                 <p>Jakarta & Coke <br> (Coke)</p>
@@ -81,78 +83,88 @@
                 <p>Limoncello <br> (Lemon Drop Mix & Tonic)</p>
                 <p>Endgame <br> (OG, 7ml, Grapefruit & Tonic)</p>
                 <p>Apocalypto <br> (Limoncello, Orange & Grapefruit)</p>
-            </div>
+            </div>-->
         </div>
         <h5 class="py-3 neonTextRed">Mocktails</h5>
         <div class="row">
             <div class="col-lg-6">
-                <p>Jakarta & Coke <br> (Coke)</p>
-                <p>New Fashioned <br> (Old Fashion Mix, Tonic & Orange)</p>
-                <p>Saigon Mule <br> (Ginger Beer)</p>
-                <p>Tea & Tonic <br> (Tonic Water)</p>
-                <p>Kosmopolitan <br> (Cosmo & Tonic)</p>
-                <p>Markarita <br> (Margarita Mix & Tonic)</p>
+                <p>ANY SODA <br> Can be mixed with Extract</p>
+                <p>JAKARTA & COKE <br> (Coke)</p>
+                <!--<p>New Fashioned <br> (Old Fashion Mix, Tonic & Orange)</p>-->
+                <p>SAIGON MULE <br> (Ginger Beer)</p>
+                <p>TEA & TONIC <br> (Tonic Water)</p>
+                <p>KOSMOPOLITAN <br> (Cosmo & Tonic)</p>
+                <p>MARKARITA <br> (Margarita Mix & Tonic)</p>
+                <p>$7 - TEA SHOTS <br> (w/ Choice of Mixer)</p>
             </div>
             <div class="col-lg-6">
-                <p>Limoncello <br> (Lemon Drop Mix & Tonic)</p>
-                <p>Superjuice <br> (Blue Lemonade Sunkist)</p>
-                <p>Witch Upon <br> (Sour Apple & Tonic)</p>
-                <p>Crystal Lake <br> (Blue Raspberry & Sprite)</p>
-                <p>Vampire's Kiss <br> (Strawberry & Sprite)</p>
-                <p>Apocalypto <br> (Limoncello, Orange & Grapefruit)</p>
+                <p>LIMONCELLO <br> (Lemon Drop Mix & Tonic)</p>
+                <p>SUPERJUICE <br> (Blue Lemonade Sunkist)</p>
+                <p>WITCH UPON <br> (Sour Apple & Tonic)</p>
+                <p>CRYSTAL LAKE <br> (Blue Raspberry & Sprite)</p>
+                <p>VAMPIRE'S KISS <br> (Strawberry & Sprite)</p>
+                <p>APOCALYPTO <br> (Limoncello, Orange & Tonic)</p>
+                <p>SATAN KILLER <br> (Blood Orange, Bitters, Cherry, & Tonic)</p>
             </div>
         </div>
+        <h5 class="py-3 neonTextRed">Mt. Kaiju Mocktails</h5>
+        <p>GODZILLA <br> (Sour Apple Mix, Apple Soda, & Extract)</p>
+        <p>ULTRAMAN <br> (Strawberry Mix, Tonic, Grapefruit Juice, & Extract)</p>
+        <p>KAMEN RIDER <br> (Blue Razz Mix, Ginger Beer, Lime, & Extract)</p>
+        <p>CAPTAIN GORDON <br> (Guarana Soda & Extract w/ a Tajin rim)</p>
         <div class="mt-5">
-            <p>(Extract Boost $3, OGs $5, Flavored Teas $6, Mocktails $7, Apocalypto or Nitro $8, and Endgame $13)</p>
+            <p>($3 - EXTRACT BOOST, $5/$10 - OGs, $6/$12 - FLAVORED TEAS, $7/$13 - MOCKTAILS, $8 - APOCALYPTO/SATAN KILLER, & $13 - ENDGAME)</p>
         </div>
     </section>
     <section id="other-beverages" class="pb-5 text-center border-top">
-        <h4 class="pt-5 pb-3">Other Beverages</h4>
+        <h4 class="pt-5 pb-3">Other Beverages & Snacks</h4>
         <div class="row">
             <div class="col-lg-6">
-                <h5 class="py-3 neonTextGreen">Hot Teas</h5>
-                <p>Chamomile</p>
-                <p>Earl Grey</p>
-                <p>English Breakfast</p>
-                <p>Ginger</p>
-                <p>Green Tea</p>
-                <p>Hibiscus</p>
-                <p>Mate</p>
-                <p>Moringa</p>
-                <p>Oolong</p>
-                <p>Rooibos</p>
-                <p>
-                    (We only use Oat Milk for dairy)
-                </p>
-            </div>
-            <div class="col-lg-6">
                 <h5 class="py-3 neonTextOrange">Boba Tea & Drinks</h5>
-                <p>Boba Tea</p>
-                <p>Celcius / Red Bull</p>
-                <p>Coke</p>
-                <p>Sprite</p>
-                <p>Moutain Dew</p>
-                <p>MD Gingerbread</p>
-                <p>MD VooDew</p>
-                <p>Diet Coke</p>
-                <p>Sunkist Blue Lemonade</p>
-                <p>Cherry Coke</p>
-                <p>Vernors Ginger</p>
+                <p>$2 - HOT TEA</p>
+                <p>$2 - SODA</p>
+                <p>$3 - ALANI / RED BULL</p>
+                <p>$4 - BOBA TEA</p>
             </div>
             <div class="col-lg-6">
                 <h5 class="py-3 neonText">Pitchers</h5>
-                <p>$31 - Kava or Mocktails</p>
-                <p>$26 - Happy Teas</p>
+                <p>$31 - KAVA or MOCKTAILS</p>
+                <p>$26 - HAPPY TEAS
+            </div>
+
+        </div>
+                <h5 class="py-3 neonTextWhite">Homemade Bar Snacks & Infused Edibles</h5>
+                <p>$1.75 - CANDY BARS</p>
+                <p>$1.00 - UTZ CHIPS <br> Original, Crab CHips, Sour Cream & Onion, Salt & Vinegar, Various Rotating Utz Chips</p>
+                <p>$7 - KRISTEN'S KRAVINGS <br> Infused Gummy Bears, Kavamisu, Cake Pops, Rice Krispy Treats</p>
+                <p>$4 - KRISTEN'S OMFG COOKIE</p>
+                <p>$4 - KRISTEN'S VEGAN BROWNIE</p>
+                <p>$5 - FLEX BITES <br> PB Choco Chip, Cashew Cookie, Cinnamon Bliss</p>
+                <p>$3 - BIG BAG of COOKIES <br> Chips Ahoy, Oreos, Ritz Cheese, Ritz PB, Nutter Butter, Teddy Grahams</p>
+                <p>$1 - MINI TREATS <br> (2 for $1) Rice Krispy Treats, Meji Pandas Raspberry Short Knotts Cookies</p>
+                <p>$3.50 - QUEST NUTRITION / PURE PROTEIN BARS <br> White Chocolate Raspberry, Chocolate Chip Cookie Dough, Cookies & Cream</p>
+    </section>
+    <section id="specials-discounts" class="pb-5 text-center border-top">
+        <h4 class="pt-5 pb-3">Specials & Discounts</h4>
+        <div class="row">
+            <div class="col-lg-6">
+                <h5 class="py-3 neonTextRed">Specials</h5>
+                <p>FREE BULA @ 11:11 p.m. every night <br> Get a large Happy Tea or Mocktail for the price of a small after 11:11 p.m.</p>
+                <p>Fire Fighters, Police Officers, Paramedics, Active Military, & Teachers <br> ALWAYS get 25% off regular Kava & Teas</p>
+                <p>Sundays from 7 p.m. until Closing <br> College students w/ ID get 50% off regular Kava & Teas.</p>
+                <p>Ladies on Tuesdays <br> Get 50% off on regular Kava & Teas for Ladies Night.</p>
             </div>
             <div class="col-lg-6">
-                <h5 class="py-3 neonTextWhite">Homemade Bar Snacks & Infused Edibles</h5>
-                <p>$4 - OMFG Cookie</p>
-                <p>$4 - Vegan Brownie</p>
-                <p>$6 - Kavamisu</p>
-                <p>$6 - Bula Buddies</p>
-                <p>$6 - Tasty Kake Truffles</p>
-                <p>$6 - K Bears</p>
-                <p>$6 - Sour K.O.s</p>
+                <h5 class="py-3 neonTextGreen">Discounts</h5>
+                <p>HAPPY HOUR <br> Monday-Friday, 4-7 p.m., & ALL-DAY Sunday</p>
+                <p>$3.75/$7.50 HH OG HAPPY TEA <br> (reg. price $5/$10)</p>
+                <p>$4.50/$9 HH SPECIAL TEA <br> (reg. price $6/$12)</p>
+                <p>$5.25/$9.75 HH MOCKTAIL <br> (reg. price $7/$13)</p>
+                <p>$3 HH SINGLE KAVA <br> (reg. price $4)</p>
+                <p>$5.25 HH DOUBLE KAVA <br> (reg. price $7)</p>
+                <p>$6.75 HH FLAVORED KAVA <br> (reg. price $9)</p>
+                <p>$3.50/$6.75 HH COLD BREW<br> (reg. price $5/$9)</p>
+                <p>$5.25/$9.75 HH FLAVORED COLD BREW <br> (reg. price $7/$13)</p>
             </div>
         </div>
     </section>
